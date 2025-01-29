823a9c0d99022b3b5842056d59e7938394d1755e
chaitanya-0799 <146313463+chaitanya-0799@users.noreply.github.com>
2025-01-29 17:45:37 +0530

    Update demoPython.groovy

--- vars/demoPython.groovy
+++ vars/demoPython.groovy
@@ -19,7 +19,7 @@ def call( String dockerCred = 'a' ,String githubURL = 'a', String gitBranch = 'a
             }
             stage('Build'){
                 steps {
-                    sh "docker build -t ${dockerImage} -f build/Dockerfile ."
+                    sh "docker build -t ${dockerImage}:${dockerTag} -f build/Dockerfile ."
                 }
             }
 
