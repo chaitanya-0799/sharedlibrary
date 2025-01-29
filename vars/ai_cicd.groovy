e1df71f555fc5d1f3f8c0b016490fffcbf61ab8b
chaitanya-0799 <146313463+chaitanya-0799@users.noreply.github.com>
2025-01-29 20:45:15 +0530

    Create ai_cicd.groovy

---
+++ vars/ai_cicd.groovy
@@ -0,0 +1,55 @@
+def call( String dockerCred, String githubURL, String gitBranch, String dockerfilePath, 
+          String dockerImage, String docTag, String containerName, String containerPort, String applicationPort ) {
+
+    pipeline {
+        environment {
+            DOCKER_CRED = "${dockerCred}"
+            GITHUB_URL = "${githubURL}"
+            GIT_BRANCH = "${gitBranch}"
+            PATH_FILE = "${dockerfilePath}"
+            DOCKER_IMAGE = "${dockerImage}"
+            DOCKER_TAG = "${docTag}${BUILD_NUMBER}" 
+            CONTAINER_NAME = "${containerName}"
+            CONT_PORT = "${containerPort}"
+            APP_PORT = "${applicationPort}"
+        }
+
+        agent any
+
+        stages {
+            stage('Git Checkout') {
+                steps {
+                    checkout scm
+                }
+            }
+
+            stage('Build Docker Image') {
+                steps {
+                    sh "docker build -t ${DOCKER_IMAGE}:${DOCKER_TAG} -f ${PATH_FILE} ."
+                }
+            }
+
+            stage('Push to Docker Registry') {
+                steps {
+                    script {
+                        docker.withRegistry('https://index.docker.io/v1/', DOCKER_CRED) {
+                            sh "docker push ${DOCKER_IMAGE}:${DOCKER_TAG}"
+                        }
+                    }
+                }
+            }
+
+            stage('Deploy Container') {
+                steps {
+                    script {
+                        sh """
+                        docker ps -q --filter name=${CONTAINER_NAME} | grep -q . && docker stop ${CONTAINER_NAME} || true
+                        docker ps -aq --filter name=${CONTAINER_NAME} | grep -q . && docker rm ${CONTAINER_NAME} || true
+                        docker run -d -p ${CONT_PORT}:${APP_PORT} --name ${CONTAINER_NAME} ${DOCKER_IMAGE}:${DOCKER_TAG}
+                        """
+                    }
+                }
+            }
+        }
+    }
+}
